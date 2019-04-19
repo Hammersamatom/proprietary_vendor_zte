731fc2d3ef93ed2bea831488b65c76b7517fbc85
laci200270 <laci200270@gmail.com>
2019-04-19 22:10:49 +0000

    axon7: update gps blobs from nitrogen os's oneplus3 repo
    Change-Id: Ie89a62355bf68c139a2c0b8b0f88e811929ce264

--- axon7/axon7-vendor.mk
+++ axon7/axon7-vendor.mk
@@ -798,6 +798,7 @@ PRODUCT_COPY_FILES += \
     vendor/zte/axon7/proprietary/vendor/lib64/vendor.qti.gnss@1.2.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.gnss@1.2.so \
     vendor/zte/axon7/proprietary/vendor/lib64/vendor.qti.gnss@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.gnss@2.0.so \
     vendor/zte/axon7/proprietary/vendor/lib64/vendor.qti.hardware.alarm@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.alarm@1.0.so \
+    vendor/zte/axon7/proprietary/lib64/android.hardware.gnss@8.0.so:system/lib64/android.hardware.gnss@8.0.so 
 
 
 PRODUCT_PACKAGES += \
